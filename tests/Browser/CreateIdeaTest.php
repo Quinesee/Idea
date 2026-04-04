5a6c620fea0ea7d5f25601d6b7785c18dbf77e82
Joshua Norvelle <joshua.norvelle@gmail.com>
2026-04-03 22:47:15 -0500

    Updating test to include steps

--- tests/Browser/CreateIdeaTest.php
+++ tests/Browser/CreateIdeaTest.php
@@ -14,13 +14,19 @@ it('creates a new idea', function () {
         ->click('@submit-new-link-button')
         ->fill('@new-link', 'https://example.com')
         ->click('@submit-new-link-button')
+        ->fill('@new-step', 'Step 1')
+        ->click('@submit-new-step-button')
+        ->fill('@new-step', 'Step 2')
+        ->click('@submit-new-step-button')
         ->click('Create')
         ->assertPathIs('/ideas');
 
-    expect($user->ideas()->first())->toMatchArray([
+    expect($idea = $user->ideas()->first())->toMatchArray([
         'title' => 'Example title',
         'status' => 'in_progress',
         'description' => 'Example description',
         'links' => ['https://test.com', 'https://example.com'],
     ]);
+
+    expect($idea->steps()->count())->toBe(2);
 });
